a293ef72a2a55c2792c233dbaf38a3eac06fabf8
Massimo Miserendino <thgdfthrplay@gmail.com>
2015-02-21 21:16:07 +0100

    exercise 9 working

--- Miserendino_810130/esercizio9/esercizio9.0.cpp
+++ Miserendino_810130/esercizio9/esercizio9.0.cpp
@@ -1,12 +1,13 @@
 #include <random>
 #include <iostream>
-#include "lib.hpp"
+#include "esperimentoprisma.hpp"
 #include "TCanvas.h"
 #include "TApplication.h"
 #include "TH2D.h"
 #define N 10000
 
 int main(int argc, char **argv){
+double dm1[N],dm2[N];
 double theta1[N],theta2[N],theta0[N];
 TApplication myapp("app",NULL,NULL); // crea l'interfaccia grafica
   TCanvas tela1;
@@ -30,13 +31,13 @@ std::cout << theta1[i] << '\t' << theta2[i] << std::endl;
    for (int i=0;i<N; ++i){
    histo3.Fill(theta2[i]); // riempe l'istogramma
    }
-   //histo3.GetXaxis()->SetTitle("X"); // da il nome all'asse X
-   //histo3.GetYaxis()->SetTitle("Y"); // da il nome all'asse Y
+   histo3.GetXaxis()->SetTitle("X[rad]"); // da il nome all'asse X
+   histo3.GetYaxis()->SetTitle("Y"); // da il nome all'asse Y
    histo3.Draw();
   tela2.cd(2);            // sposta il cursore sulla prima parte della canvas
    TH1D histo2("theta1","theta1",100,foo.Gettheta1in()-1E-3, foo.Gettheta1in()+1E-3); // crea l'istogramma
-   //histo2.GetXaxis()->SetTitle("X"); // da il nome all'asse X
-   //histo2.GetYaxis()->SetTitle("Y"); // da il nome all'asse Y
+   histo2.GetXaxis()->SetTitle("X[rad]"); // da il nome all'asse X
+   histo2.GetYaxis()->SetTitle("Y"); // da il nome all'asse Y
    for (int i=0;i<N; ++i){
    histo2.Fill(theta1[i]); // riempe l'istogramma
    }
@@ -46,12 +47,64 @@ std::cout << theta1[i] << '\t' << theta2[i] << std::endl;
    for (int i=0;i<N; ++i){
    histo4.Fill(theta0[i]); // riempe l'istogramma
    }
-   //histo3.GetXaxis()->SetTitle("X"); // da il nome all'asse X
-   //histo3.GetYaxis()->SetTitle("Y"); // da il nome all'asse Y
+   histo4.GetXaxis()->SetTitle("X[rad]"); // da il nome all'asse X
+   histo4.GetYaxis()->SetTitle("Y"); // da il nome all'asse Y
    histo4.Draw();
+   // definisco un altra tela per i dm
+  TCanvas tela3("c2","c2",1200,400);
+  tela3.Divide(3,1);
+  tela3.cd(1);
+   // calcolo dei residui per dm1,2
+      TH2F corrdm12("corrdm12","Residui dm1,2",100,-0.002,0.002,100,-0.002,0.002);
+  double coor2,coor1;
+  // *******************************************************************************************
+   TH1D histo5("dm1","dm1",100,(foo.Gettheta1in()-foo.Gettheta0in())-1E-3,(foo.Gettheta1in()-foo.Gettheta0in())+1E-3); // crea l'istogramma
+   for (int i=0;i<N; ++i){
+     foo.Esegui();
+     foo.Analizza();
+  dm1[i]=foo.Getdm1();
+  dm2[i]=foo.Getdm2();
+  coor1=foo.Getdm1()-foo.Getdm1in();
+  coor2=foo.Getdm2()-foo.Getdm2in();
+   corrdm12.Fill(coor1,coor2);
+   histo5.Fill(dm1[i]); // riempe l'istogramma
+#ifdef DEBUG
+   std::cout << coor1 << " " << coor2 << std::endl;
+   std::cout << dm1[i] <<  std::endl;
+#endif
+   }
+   histo5.GetXaxis()->SetTitle("X[rad]"); // da il nome all'asse X
+   histo5.GetYaxis()->SetTitle("Y"); // da il nome all'asse Y
+   histo5.Draw();
+   // dm2 
+  tela3.cd(2);
+   TH1D histo6("dm2","dm2",100,(foo.Gettheta2in()-foo.Gettheta0in())-1E-3,(foo.Gettheta2in()-foo.Gettheta0in())+1E-3); // crea l'istogramma
+   for (int i=0;i<N; ++i){
+   histo6.Fill(dm2[i]); // riempe l'istogramma
+#ifdef DEBUG
+   std::cout << dm2[i] <<  std::endl;
+#endif
+   }
+   histo6.GetXaxis()->SetTitle("X[rad]"); // da il nome all'asse X
+   histo6.GetYaxis()->SetTitle("Y"); // da il nome all'asse Y
+   histo6.Draw();
    
+   corrdm12.GetXaxis()->SetTitle("Discrepanza dm1 [rad]");
+   corrdm12.GetYaxis()->SetTitle("Discrepanza dm2 [rad]");
+
+   tela3.cd(3);
+   corrdm12.Draw();
+
+
+
+
+
+
+
+
    myapp.Run(true);
    tela1.Close();
    tela2.Close();
+   tela3.Close();
   return 0;
 }
---
+++ Miserendino_810130/esercizio9/esperimentoprisma.hpp
@@ -0,0 +1,110 @@
+#ifndef ESPERIMENTOPRISMA_HPP 
+#define ESPERIMENTOPRISMA_HPP
+#include <cmath>
+#include <random>
+using namespace std;
+#define M_PI           3.14159265358979323846  /* pi */
+
+  std::default_random_engine generator;
+
+class EsperimentoPrisma {
+  public:
+    EsperimentoPrisma();
+    void Esegui();
+    void Analizza();
+    double Gettheta1();
+    double Gettheta0();
+    double Gettheta2();
+    double Gettheta1in();
+    double Gettheta2in();
+    double Getdm1();
+    double Getdm2();
+    double Getdm1in();
+    double Getdm2in();
+    double Gettheta0in();
+  private:                
+    double lambda1,lambda2; 
+    double alpha,sigmat;
+    double a_input, a_misurato;
+    double b_input, b_misurato;
+    double n1_input, n1_misurato;
+    double n2_input, n2_misurato;
+    double t0_input, t0_misurato;
+    double t1_input, t1_misurato;
+    double t2_input, t2_misurato;
+    double dm1,dm2;
+    double dm1in,dm2in;
+};
+EsperimentoPrisma::EsperimentoPrisma() :
+  lambda1(579.1E-9),
+  lambda2(404.7E-9),
+  alpha(60*M_PI/180.),
+ sigmat(0.3E-3),
+  a_input(2.7),
+  b_input(60000E-18)
+{
+  //calcolo degli indici di rifrazione attesi
+  n1_input = sqrt(a_input+b_input/(pow(lambda1,2)));
+  n2_input = sqrt(a_input+b_input/(pow(lambda2,2)));
+// calcolo dei valori attesi degli angoli misurati
+  t0_input = M_PI/2;  //theta0 e' arbitrario
+  dm1in = 2.*asin(n1_input*sin(0.5*alpha))-alpha;
+  t1_input = t0_input+dm1;
+  dm2in = 2.*asin(n2_input*sin(0.5*alpha))-alpha;
+  t2_input = t0_input+dm2;
+}
+void EsperimentoPrisma::Esegui(){
+  std::normal_distribution<double> distribution0(t0_input,sigmat);        // in questo modo uso la STD per creare le grandezze come se fossero una gaussiana cetrata dove
+  t0_misurato=distribution0(generator);                                   // voglio io e con RMS che voglio io
+  std::normal_distribution<double> distribution1(t1_input,sigmat);
+  t1_misurato=distribution1(generator);
+  std::normal_distribution<double> distribution2(t2_input,sigmat);
+  t2_misurato=distribution2(generator);
+}
+double EsperimentoPrisma::Gettheta1(){
+  return t1_misurato;
+}
+double EsperimentoPrisma::Gettheta2(){
+  return t2_misurato;
+}
+
+double EsperimentoPrisma::Gettheta0(){
+  return t0_misurato;
+}
+
+double EsperimentoPrisma::Gettheta0in(){
+  return t0_input;
+}
+
+double EsperimentoPrisma::Gettheta1in(){
+  return t1_input;
+}
+
+double EsperimentoPrisma::Gettheta2in(){
+  return t2_input;
+}
+
+double EsperimentoPrisma::Getdm1(){
+  return dm1;
+}
+double EsperimentoPrisma::Getdm2(){
+  return dm2;
+}
+
+double EsperimentoPrisma::Getdm1in(){
+  return dm1in;
+}
+double EsperimentoPrisma::Getdm2in(){
+  return dm2in;
+}
+
+void EsperimentoPrisma::Analizza(){
+dm1=t1_misurato-t0_misurato;
+dm2=t2_misurato-t0_misurato;
+n1_misurato=(sin(dm1+alpha)/2)/sin(alpha/2);
+n2_misurato=(sin(dm2+alpha)/2)/sin(alpha/2);
+a_misurato=pow(lambda2,2)*pow(n2_misurato,2)-pow(n1_misurato,2)*pow(lambda2,2);
+b_misurato=(pow(n2_misurato,2)-pow(n1_misurato,2))/(1/pow(lambda2,2)-1/pow(lambda1,2));
+}
+
+#endif
